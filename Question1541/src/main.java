2bb7d777075dc9e48a31c8039371b68262f6158c
JYNG <BLU@JYNGui-MacBook-Pro.local>
2016-07-12 14:00:31 +0900

    go

--- Question1541/src/main.java
+++ Question1541/src/main.java
@@ -27,7 +27,7 @@ public class main {
 	
 	}
 	
-	public static void first(String str, char[] arr) {
+	public static int first(String str, char[] arr) {
 		//val = arr[0];
 		
 		String digitString = arr[0] + "";
@@ -54,7 +54,9 @@ public class main {
 				digitString = digitString + arr[i];
 			}
 			indexOf = str.length();
+			val = Integer.valueOf(digitString);
 			
+			return 0;
 		}
 		
 		System.out.println(digitString);
@@ -71,16 +73,97 @@ public class main {
 			arr[i] = ch;
 			}
 		
-		System.out.print(arr);
+		System.out.println(arr);
 		val = Integer.valueOf(digitString);
 		
+		calculate(str, arr);
+		
+		return 0;
 	}
 	
 	
-	public void calculate(String str , char[] arr) {
+	public static int calculate(String str , char[] arr) {
 		
 		
+		char operator = arr[0];
 		
+		str = str.substring(1, str.length());
+		
+		arr = null;
+		
+		arr = new char[str.length()];
+		
+		for (int i = 0; i < str.length(); i++) {
+
+			char ch = 0;
+			ch = str.charAt(i);
+			arr[i] = ch;
+			}
+		
+		String digitString = arr[0] + "";
+		
+		int indexOfPlus = str.indexOf("+");
+		int indexOfMinus = str.indexOf("-");
+		int indexOf;
+		
+		
+		if (indexOfPlus > indexOfMinus && indexOfMinus != -1 || indexOfMinus > 0 && indexOfPlus == -1) {
+			for (int i = 1; i < indexOfMinus; i++) {
+				digitString = digitString + arr[i];
+			}
+			indexOf = indexOfMinus;
+		}
+		else if (indexOfPlus < indexOfMinus && indexOfPlus != -1 || indexOfPlus > 0 && indexOfMinus == -1) {
+			for (int i = 1; i < indexOfPlus; i++) {
+				digitString = digitString + arr[i];
+			}
+			indexOf = indexOfPlus;
+		} else {
+			for (int i = 1; i < str.length(); i++) {
+				digitString = digitString + arr[i];
+			}
+			
+			if (operator == '+') {
+				val = val + Integer.valueOf(digitString);
+				System.out.println(val);
+				
+			} else if (operator == '-'){
+				val = val - Integer.valueOf(digitString);
+				System.out.println(val);
+				
+			}
+			
+			
+			
+			return 0;
+		}
+		
+		if (operator == '+') {
+			val = val + Integer.valueOf(digitString);
+			System.out.println(val);
+			
+		} else if (operator == '-'){
+			val = val - Integer.valueOf(digitString);
+			System.out.println(val);
+			
+		}
+		
+		str = str.substring(indexOf, str.length());
+		
+		arr = null;
+		
+		arr = new char[str.length()];
+		
+		for (int i = 0; i < str.length(); i++) {
+
+			char ch = 0;
+			ch = str.charAt(i);
+			arr[i] = ch;
+			}
+		
+		calculate(str, arr);
+		
+		return 0;
 	}
 	
 	
